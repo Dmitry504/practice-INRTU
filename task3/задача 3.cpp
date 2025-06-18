8aae9d34b981a8c5bdc9b33b96115e29b46c14e0
Dmitry504 <d57656241@gmail.com>
2025-06-18 18:30:33 +0800

    add task3

---
+++ task3/задача 3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+
+
+int main() {
+  int n, k;
+  std::cin >> n >> k;
+
+  std::vector<long long> streets(n + 1, 0); // улицы от 1 до n
+
+  for (int i = 0; i < k; ++i) {
+    int type;
+    std::cin >> type;
+    if (type == 1) {
+      int ix;
+      long long x;
+      std::cin >> ix >> x;
+      streets[ix] += x;
+    }
+    else if (type == 2) {
+      int u, r;
+      std::cin >> u >> r;
+      long long sum = 0;
+      for (int j = u; j <= r; ++j) {
+        sum += streets[j];
+      }
+      std::cout << sum << std::endl;
+    }
+  }
+
+  return 0;
+}
